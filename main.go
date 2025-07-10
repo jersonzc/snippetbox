4feef194d0c3c13831e24fc59645ac631a5fa088
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-10 15:13:29 -0500

    feat: start server

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main() {
-	fmt.Println("Hello world!")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func main() {
+	port := 4001
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Printf("Starting server on port %d", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
+	log.Fatal(err)
+}
