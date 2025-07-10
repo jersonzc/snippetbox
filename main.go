d81b6ba606243b53e6feb9c3672939c7c6d2d1d8
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-10 18:10:38 -0500

    feat: add Allow header

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		w.Write([]byte("Method not allowed"))
 		return
