aa802654c7bd52a5865f9c9283edb1bf48361087
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-10 19:19:03 -0500

    feat: read static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,7 +9,10 @@ import (
 func main() {
 	port := 4001
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
