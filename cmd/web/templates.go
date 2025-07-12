f656777c33bc9ab7932fa3eb2f7bfbf0b731bc7b
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-12 12:20:16 -0500

    feat: format time

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,21 @@ import (
 	"html/template"
 	"path/filepath"
 	"snippetbox/internal/models"
+	"time"
 )
 
 type templateData struct {
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 	CurrentYear int
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -23,7 +32,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
