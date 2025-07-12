855f2c3585847501b3ecc0cb00ce039463e55b1c
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-12 13:06:56 -0500

    feat: add headers middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,14 +2,15 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return secureHeaders(mux)
 }
