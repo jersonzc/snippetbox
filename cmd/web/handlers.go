891c255343457641ed128f5ab8794fc669639062
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-07-15 15:10:11 -0500

    feat: check fields

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"snippetbox/internal/models"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 )
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -51,6 +53,30 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	fieldErrors := make(map[string]string)
+
+	// check title
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors["title"] = "This field cannot be longer than 100 characters"
+	}
+
+	// check content
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	// check expires
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must be either 1 or 7 or 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
 		return
 	}
 
