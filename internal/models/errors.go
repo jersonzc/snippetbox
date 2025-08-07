de89612435ad451ccfd492780c23ae30b2c9285a
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-08-07 11:33:49 -0500

    feat: authenticate user

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -3,6 +3,7 @@ package models
 import "errors"
 
 var (
-	ErrNoRecord       = errors.New("models: no matching record found")
-	ErrDuplicateEmail = errors.New("models: duplicate email")
+	ErrNoRecord           = errors.New("models: no matching record found")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
 )
